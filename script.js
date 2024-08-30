fa1fefa25e379545ded97d3b1ac69c13f5fabd8f
Zim <h.harisfadzilah@lancaster.ac.uk>
2024-08-30 15:35:07 +0100

    added image slideshow

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 $(function(){
-    var x = 0;
-    var y = 0;
-    setInterval(function(){
-      x -=3;
-      y -=2;
-      $('#bg').css('background-position', x + 'px ' + y +'px');
-    }, 50);
-  });
+  var x = 0;
+  var y = 0;
+  setInterval(function(){
+    x -=3;
+    y -=2;
+    $('#bg').css('background-position', x + 'px ' + y +'px');
+  }, 50);
+});
+
+let slideIndex = 0;
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
